33258d8cc7956f9455a97e6f784e454c5c2e1320
liuyong <583633876@qq.com>
2016-06-10 15:54:19 +0800

    LuaBase

---
+++ BaseLua/NumberDemo.lua
@@ -0,0 +1,11 @@
+--
+-- Created by IntelliJ IDEA.
+-- User: liuyong
+-- Date: 2016/6/10
+-- Time: 15:48
+-- To change this template use File | Settings | File Templates.
+--
+a = 5.026
+i = a % 0.1
+print (i)
+
