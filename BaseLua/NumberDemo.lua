7cfbf196e9da119d2904d8602905ce189b76d423
tcliuyong <583633876@qq.com>
2016-06-10 21:02:39 +1200

    LuaPath

--- BaseLua/NumberDemo.lua
+++ BaseLua/NumberDemo.lua
@@ -6,6 +6,13 @@
 -- To change this template use File | Settings | File Templates.
 --
 a = 5.026
+--取小数部分
 i = a % 0.1
+--保留整数部分
+f = a % 1
+--保留二位小数
+tf = a %0.01
 print (i)
+print (f)
+print (tf)
 
